ae698761076426767378c332bd4c98744b27ca9b
tlaureen <tlaureen@student.21-school.ru>
2022-02-14 00:21:58 +0500

    Check Faled test

--- GetMessage_test.go
+++ GetMessage_test.go
@@ -3,7 +3,7 @@ package solution
 import "testing"
 
 func TestGetMessage(t *testing.T) {
-	if GetMessage() != "Hello 🗺️ !" {
+	if GetMessage() != " Hello 🗺️ !" {
 		t.Errorf("Got-'%q' Want 'Hello 🗺️ !'", GetMessage())
 	}
 }
